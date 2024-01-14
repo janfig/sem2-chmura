6826a3f73dc8d850bb9043b36dfb256a0fd6c277
Jan Figura <jan.figura@300devs.com>
2024-01-14 07:48:23 +0100

    update start.sh

--- zad2/start.sh
+++ zad2/start.sh
@@ -2,3 +2,4 @@
 minikube kubectl -- create ns zad2
 find ./config -name '*.yaml' -exec minikube kubectl -- apply -f {} \;
 minikube kubectl -- apply -f operator-stepcd.yaml
+curl -H "Host: lab2.zad" $(minikube service -n zad2 webapp-svc --url)
